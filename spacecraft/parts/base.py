d0cce56ef60f5aea7ce8234057f9dcdc18d98919
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2023-11-30 10:07:51 +0100

    modfiy base part

--- spacecraft/parts/base.py
+++ spacecraft/parts/base.py
@@ -36,6 +36,10 @@ parts_manager = _PartsManager()
 
 
 class PartStatus(enum.Enum):
+    """
+    the status ID are in descending order according to the severity level (0 is the most severe)
+    """
+
     OFFLINE = 1
     ERROR = 2
     NOMINAL = 3
@@ -46,6 +50,9 @@ class PartStatus(enum.Enum):
         ERROR: "red",
         OFFLINE: "gray",
     }
+
+    def __lt__(self, other: Self) -> bool:
+        return self.value < other.value
 
     @property
     def color(self) -> str:
@@ -61,8 +68,13 @@ class BasePart(abc.ABC):
         self._part_id: str = ""
         parts_manager.add(self)
 
-        # self.dependencies: set[BasePart] = set()
-        # self.dependants: set[BasePart] = set()
+    def validate(self) -> None:
+        """
+        Validate the configuration of the part.
+        Raises a corresponding exception if the config is invalid.
+        :exception ValueError
+        """
+        pass
 
     @property
     def name(self) -> str:
@@ -72,14 +84,41 @@ class BasePart(abc.ABC):
     def part_id(self) -> str:
         return self._part_id
 
+    @property
     @abc.abstractmethod
     def dependencies(self) -> set[Self]:
         pass
 
     @property
-    def status(self) -> PartStatus:
+    def __power_status(self) -> PartStatus:
+        """
+        The part status solely based on the power state (on or off) of the part.
+        """
         if not self.__power_state:
             return PartStatus.OFFLINE
+        return PartStatus.NOMINAL
+
+    @property
+    def __dependency_status(self) -> PartStatus:
+        """
+        The part status solely based on the dependencies.
+        If any of the dependencies are in a non-nominal state, the device itself is in state :code:`PartStatus.ERROR`.
+        """
+        for dependency in self.dependencies:
+            if dependency.status != PartStatus.NOMINAL:
+                return PartStatus.ERROR
+        return PartStatus.NOMINAL
+
+    @property
+    def _part_specific_status(self) -> PartStatus:
+        """
+        The part status solely based on the properties of the specific part.
+        """
+        return PartStatus.NOMINAL
+
+    @property
+    def status(self) -> PartStatus:
+        return min(self.__power_status, self.__dependency_status, self._part_specific_status)
 
     def power_on(self) -> None:
         self.__power_state = True
