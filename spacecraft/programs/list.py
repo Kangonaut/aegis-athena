a26e9eaf103e6393c9bcf64e1f6e35c387e89e93
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2023-12-01 20:21:24 +0100

    modify list program to output sorted by part ID

--- spacecraft/programs/list.py
+++ spacecraft/programs/list.py
@@ -24,6 +24,7 @@ class ListProgram(BaseProgram):
 
     def __handle_list_parts(self) -> None:
         parts: set[BasePart] = self._parts_manager.get_all()
+        parts: list[BasePart] = sorted(parts, key=lambda part: part.part_id)
         for part in parts:
             self._display.print(f"{part.part_id:<10} {part.name:<30} [[ {part.status:<7} ]]")
 
