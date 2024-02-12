33016378e2e5dff32b9366958d9d9281ffabf28e
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-02-12 12:24:17 +0100

    add eval on MARS-v1

---
+++ rag/eval_utils.py
@@ -0,0 +1,53 @@
+from trulens_eval.app import App
+from trulens_eval import TruLlama, Feedback
+from trulens_eval.feedback import Groundedness, OpenAI
+
+from llama_index.query_engine import BaseQueryEngine
+
+import numpy as np
+from typing import Generator, Iterator
+
+
+def get_eval_dataset_gen(dataset_path: str) -> Generator[str, None, None]:
+    with open(dataset_path, "r") as file:
+        for line in file:
+            yield line
+
+
+def get_triad_of_metrics_eval_app(app_id: str, query_engine: BaseQueryEngine) -> App:
+    # feedback provider
+    llm = OpenAI()  # (model_engine="gpt-4")
+
+    # RAG triad of metrics
+    context = App.select_context(query_engine)
+
+    # Groundedness: Is the response backed up by the provided context?
+    grounded = Groundedness(groundedness_provider=llm)
+    f_groundedness = Feedback(
+        grounded.groundedness_measure_with_cot_reasons,
+        name="Groundedness",
+    ).on(context.collect()).on_output().aggregate(grounded.grounded_statements_aggregator)
+
+    # Answer Relevance: How relevant to the input query is the synthesized response?
+    f_answer_relevance = Feedback(
+        llm.relevance,
+        name="Answer Relevance",
+    ).on_input().on_output()
+
+    # Context Relevance: How relevant to the input query is the retrieved context?
+    f_context_relevance = Feedback(
+        llm.qs_relevance,  # relevance of a statement to a question
+        name="Context Relevance",
+    ).on_input().on(context).aggregate(np.mean)
+
+    return TruLlama(
+        app=query_engine,
+        app_id=app_id,
+        feedbacks=[f_groundedness, f_answer_relevance, f_context_relevance],
+    )
+
+
+def run_eval(query_engine: BaseQueryEngine, eval_app: TruLlama, eval_questions: Iterator[str]) -> None:
+    with eval_app as recorder:
+        for question in eval_questions:
+            query_engine.query(question)
--- rag/mars.py
+++ rag/mars.py
@@ -3,21 +3,27 @@ from llama_index import VectorStoreIndex, ServiceContext
 from llama_index.llms import OpenAI
 
 import streamlit as st
+from llama_index.vector_stores import WeaviateVectorStore
 
 from rag import weaviate_utils
 
-CLASS_NAME: str = "DocsChunk"
-
 
 @st.cache_resource()
-def get_v1() -> BaseQueryEngine:
+def get_v1(streaming=False) -> BaseQueryEngine:
+    """
+    Simple RAG setup with Weaviate as vector database (:code:`class_name="DocsChunk"`).
+    The Synthesizer uses the :code:`gpt-3.5-turbo` (:code:`temperature=0.1`) model.
+    """
+
+    weaviate_class_name: str = "MarkdownDocsChunk"
+
     weaviate_client = weaviate_utils.get_weaviate_client()
-    vector_store = weaviate_utils.as_vector_store(weaviate_client, CLASS_NAME)
+    vector_store = weaviate_utils.as_vector_store(weaviate_client, weaviate_class_name)
     index = VectorStoreIndex.from_vector_store(vector_store)
 
     llm = OpenAI(model="gpt-3.5-turbo", temperature=0.1)
     service_context = ServiceContext.from_defaults(llm=llm)
 
-    query_engine = index.as_query_engine(service_context=service_context, streaming=True)
+    query_engine = index.as_query_engine(service_context=service_context, streaming=streaming)
 
     return query_engine
