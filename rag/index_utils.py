40e11922dbb8eea3350c89000e3f25a00f2227b3
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-02-12 16:11:18 +0100

    add data loading for sentence window retrieval

--- rag/index_utils.py
+++ rag/index_utils.py
@@ -1,10 +1,12 @@
-from llama_index import Document, VectorStoreIndex, StorageContext
+from llama_index import Document, VectorStoreIndex, StorageContext, ServiceContext
 
 
-def populate_index(storage_context: StorageContext, docs: list[Document]) -> VectorStoreIndex:
+def populate_index(docs: list[Document], storage_context: StorageContext | None = None,
+                   service_context: ServiceContext | None = None) -> VectorStoreIndex:
     index = VectorStoreIndex(
         nodes=docs,
         storage_context=storage_context,
+        service_context=service_context,
         show_progress=True,
     )
     return index
--- rag/load_utils.py
+++ rag/load_utils.py
@@ -11,3 +11,25 @@ def load_data_dir(dir_path: str) -> list[Document]:
         required_exts=[".md"],
     )
     return reader.load_data()
+
+
+def group_documents(documents: list[Document], group_by: str) -> dict[str, list[Document]]:
+    """
+    Groups documents together based on a metadata attribute, identified by the :code:`group_by` parameter.
+    """
+    grouped_documents: dict[str, list[Document]] = {}
+    for document in documents:
+        key: str = document.metadata[group_by]
+        if key not in grouped_documents:
+            grouped_documents[key] = []
+        grouped_documents[key].append(document)
+    return grouped_documents
+
+
+def join_grouped_documents(grouped_documents: dict[str, list[Document]]) -> dict[str, Document]:
+    """
+    Joins grouped documents into a single document object.
+    """
+    return {key: Document(
+        text="\n\n".join(map(lambda x: x.text, documents))
+    ) for key, documents in grouped_documents.items()}
