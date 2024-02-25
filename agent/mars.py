9200e5c18787d0801a3b48e4de90f6c8ea4c17c5
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-02-25 11:59:45 +0100

    wrap MARS RAG pipeline in ReAct agent

--- agent/mars.py
+++ agent/mars.py
@@ -1,11 +1,15 @@
+from llama_index.core.agent import ReActAgent, AgentRunner
 from llama_index.core.postprocessor import SentenceTransformerRerank, MetadataReplacementPostProcessor
 from llama_index.core import VectorStoreIndex
 from llama_index.core.query_pipeline import QueryPipeline, InputComponent
+from llama_index.core.tools import ToolMetadata
 from llama_index.llms.openai import OpenAI
 from llama_index.core.prompts import PromptTemplate
 from llama_index.core.response_synthesizers import Refine
+from llama_index.core.tools.query_engine import QueryEngineTool
 
 from rag import weaviate_utils
+from rag import mars as mars_rag
 
 
 def get_v1_0() -> QueryPipeline:
@@ -13,6 +17,7 @@ def get_v1_0() -> QueryPipeline:
     similarity_top_k: int = 10
     reranked_top_n: int = 5
 
+    # weaviate
     weaviate_client = weaviate_utils.get_weaviate_client()
     weaviate_vector_store = weaviate_utils.as_vector_store(weaviate_client, weaviate_class_name)
     weaviate_index = VectorStoreIndex.from_vector_store(
@@ -24,6 +29,10 @@ def get_v1_0() -> QueryPipeline:
         alpha=0,  # 1 => vector search; 0 => BM25
     )
 
+    # sentence window retrieval
+    sentence_window_postprocessor = MetadataReplacementPostProcessor(target_metadata_key="window")
+
+    # HyDE
     hyde_prompt_template = PromptTemplate(
         "Please write a passage to answer the question\n"
         "Try to include as many key details as possible.\n"
@@ -36,9 +45,9 @@ def get_v1_0() -> QueryPipeline:
         "Question: {query_str}\n"
         "Answer: {hyde_str}"
     )
-
     hyde_llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3)
 
+    # Refine response synthesizer
     qa_prompt = PromptTemplate(
         "Context information is below.\n"
         "---------------------\n"
@@ -50,7 +59,6 @@ def get_v1_0() -> QueryPipeline:
         "Query: {query_str}\n"
         "Answer: "
     )
-
     refine_prompt = PromptTemplate(
         "The original query is as follows: {query_str}\n"
         "We have provided an existing answer: {existing_answer}\n"
@@ -65,7 +73,6 @@ def get_v1_0() -> QueryPipeline:
         "If the context isn't useful, return the original answer.\n"
         "Refined Answer: "
     )
-
     response_synthesizer_llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3)
     refine_response_synthesizer = Refine(
         text_qa_template=qa_prompt,
@@ -74,13 +81,13 @@ def get_v1_0() -> QueryPipeline:
         streaming=True,
     )
 
+    # reranker
     reranker = SentenceTransformerRerank(
         top_n=reranked_top_n,
         model="BAAI/bge-reranker-base",
     )
 
-    sentence_window_postprocessor = MetadataReplacementPostProcessor(target_metadata_key="window")
-
+    # query pipeline
     qp = QueryPipeline()
     qp.add_modules({
         "original_input": InputComponent(),
@@ -93,19 +100,44 @@ def get_v1_0() -> QueryPipeline:
         "sentence_window_postprocessor": sentence_window_postprocessor,
     })
 
+    # hyde
     qp.add_link("original_input", "hyde_prompt_template", dest_key="query_str")
     qp.add_link("hyde_prompt_template", "hyde_llm")
     qp.add_link("hyde_llm", "hyde_combined_prompt_template", dest_key="hyde_str")
     qp.add_link("original_input", "hyde_combined_prompt_template", dest_key="query_str")
     qp.add_link("hyde_combined_prompt_template", "weaviate_retriever")
 
+    # sentence window retrieval
     qp.add_link("weaviate_retriever", "sentence_window_postprocessor", dest_key="nodes")
 
+    # reranker
     qp.add_link("sentence_window_postprocessor", "reranker", dest_key="nodes")
     qp.add_link("original_input", "reranker", dest_key="query_str")
 
+    # response synthesizer
     qp.add_link("reranker", "refine_response_synthesizer", dest_key="nodes")
     qp.add_link("original_input", "refine_response_synthesizer", dest_key="query_str")
     qp.add_link("original_input", "weaviate_retriever")
 
     return qp
+
+
+def get_v2_0() -> AgentRunner:
+    rag_query_engine = mars_rag.get_v5_2()
+
+    rag_tool = QueryEngineTool(
+        query_engine=rag_query_engine,
+        metadata=ToolMetadata(
+            name="documentation_rag",
+            description="Answers questions about the Aegis Athena mission using its documentation",
+        ),
+    )
+
+    llm = OpenAI(model="gpt-3.5-turbo")
+    agent = ReActAgent.from_tools(
+        tools=[rag_tool],
+        llm=llm,
+        verbose=True,
+    )
+
+    return agent
