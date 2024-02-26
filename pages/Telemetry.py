0b8722bcb69c0b51c2ffec8a6c32c3f946035b6b
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-02-26 20:32:22 +0100

    add telemetry page

---
+++ pages/Telemetry.py
@@ -0,0 +1,20 @@
+from telemetry.base import BaseShellTrace
+from utils import telemetry_utils
+import streamlit as st
+
+manager = telemetry_utils.get_telemetry_manager()
+trace_ids: list[str] = manager.get_all_trace_ids()
+
+trace_id = st.selectbox(
+    label="Select a Trace:",
+    options=trace_ids,
+)
+
+trace = manager.get(trace_id)
+for node in trace.nodes:
+    timestamp_str: str = node.timestamp.strftime("%Y.%m.%d %H:%M:%S")
+
+    st.markdown(f"**timestamp:** {timestamp_str}")
+    container = st.container(border=True)
+    container.markdown(node.content)
+    st.divider()
