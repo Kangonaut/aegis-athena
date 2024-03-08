5ac436b65558d419e5e51a3a0200a348e289d829
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-03-08 20:26:24 +0100

    make knowledge-base-retriever inherit LlamaIndex BaseRetriever

--- agent/knowledge_base/retriever/base.py
+++ agent/knowledge_base/retriever/base.py
@@ -1,15 +1,30 @@
 from abc import ABC, abstractmethod
+from typing import Optional, Dict, List
 
-from llama_index.core.schema import NodeWithScore
+from llama_index.core import QueryBundle
+from llama_index.core.base.base_retriever import BaseRetriever
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core.schema import NodeWithScore, IndexNode
 
 from agent.knowledge_base.extract_formatter.base import BaseKnowledgeBaseExtractFormatter
 
 
-class BaseKnowledgeBaseRetriever(ABC):
+class BaseKnowledgeBaseRetriever(BaseRetriever, ABC):
     def __init__(
             self,
-            formatter: BaseKnowledgeBaseExtractFormatter
+            formatter: BaseKnowledgeBaseExtractFormatter,
+
+            callback_manager: Optional[CallbackManager] = None,
+            object_map: Optional[Dict] = None,
+            objects: Optional[List[IndexNode]] = None,
+            verbose: bool = False,
     ) -> None:
+        super().__init__(
+            callback_manager=callback_manager,
+            object_map=object_map,
+            objects=objects,
+            verbose=verbose,
+        )
         self.formatter = formatter
 
     @abstractmethod
@@ -30,3 +45,6 @@ class BaseKnowledgeBaseRetriever(ABC):
         """
         nodes = self.retrieve_raw(query)
         return self.formatter.format(nodes)
+
+    def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
+        return self.retrieve_raw(query_bundle.query_str)
